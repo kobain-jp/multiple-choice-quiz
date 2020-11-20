297275acc24cc7d8b3c6f5716ec04f5b25ca81c5
yoshifumi <yoshifumi@kobain.local>
2020-11-20 20:58:54 +0900

    fix history

--- legacy-desgined/app.js
+++ legacy-desgined/app.js
@@ -1,22 +1,5 @@
-const quizList = [];
-quizList.push({
-    question: "バックトゥザフューチャーシリーズに出演していない人は誰？",
-    choices: ["フリー(レッチリのベーシスト)", "イライジャ・ウッド(ロードオブザリングの主人公役)", "ドナルド・トランプ(元アメリカ大統領)"],
-    collectIdx: 2,
-    commentary: "ビフのモデルはトランプ大統領だが、本人は出演していない。\nフリーはマーティの同級生のニードルス、イライジャは2のカフェ80で出演しています。"
-});
-quizList.push({
-    question: "中国語でLoginはどれ",
-    choices: ["登录", "注册", "忘记密码"],
-    collectIdx: 0,
-    commentary: "登录はSign upみたいですがLoginです。\nSign upは注册。\n忘记密码はログインページに必ずあるあれです。"
-});
-quizList.push({
-    question: "栃木県宇都宮市と関係ないものはどれ？",
-    choices: ["餃子", "焼きそば", "干し芋", "レモン牛乳", "大谷石地下採掘場跡"],
-    collectIdx: 2,
-    commentary: "餃子は正嗣、焼きそばは石田屋がおすすめです。\n運動会の思い出はレモン牛乳です。\n大谷石地下採掘場跡はまるでドラクエの洞窟"
-});
+// quize data
+let quizList = [];
 
 // current question
 let question = "";
@@ -73,4 +56,49 @@ function renderQuize() {
 
 }
 
-nextQuestion();
+//model
+function loadQuize() {
+
+    return new Promise((resolve, reject) => {
+
+        //load mock data
+        const quizList = [];
+        quizList.push({
+            question: "バックトゥザフューチャーシリーズに出演していない人は誰？",
+            choices: ["フリー(レッチリのベーシスト)", "イライジャ・ウッド(ロードオブザリングの主人公役)", "ドナルド・トランプ(元アメリカ大統領)"],
+            collectIdx: 2,
+            commentary: "ビフのモデルはトランプ大統領だが、本人は出演していない。\nフリーはマーティの同級生のニードルス、イライジャは2のカフェ80で出演しています。"
+        });
+        quizList.push({
+            question: "中国語でLoginはどれ",
+            choices: ["登录", "注册", "忘记密码"],
+            collectIdx: 0,
+            commentary: "登录はSign upみたいですがLoginです。\nSign upは注册。\n忘记密码はログインページに必ずあるあれです。"
+        });
+        quizList.push({
+            question: "栃木県宇都宮市と関係ないものはどれ？",
+            choices: ["餃子", "焼きそば", "干し芋", "レモン牛乳", "大谷石地下採掘場跡"],
+            collectIdx: 2,
+            commentary: "餃子は正嗣、焼きそばは石田屋がおすすめです。\n運動会の思い出はレモン牛乳です。\n大谷石地下採掘場跡はまるでドラクエの洞窟"
+        });
+
+        resolve(quizList);
+        //reject("err");
+
+    });
+
+}
+
+//controller
+async function init() {
+    try {
+        quizList = await loadQuize();
+        nextQuestion();
+    } catch (err) {
+        questionEl.innerHTML = "クイズの取得に失敗しました。"
+        // メンテナンスページを表示
+    }
+}
+
+init();
+

29e95a228faff3191fb885c002cfe3d7d92e2a09
kobain-jp <55679081+kobain-jp@users.noreply.github.com>
2020-11-24 19:39:29 +0900

    Add files via upload

---
+++ mcquiz-easy-1124/app.js
@@ -0,0 +1,39 @@
+var question = "javascriptがjavaとついている理由は？";
+var choices = ["jvmで動くから", "javaと全く同じ構文で動くから", "当時Javaが流行っていたから"]
+var collectIdx = 2;
+var commentary = "Java is to JavaScript as Car is to Carpetと言われていれるぐらいjavaとjavascriptは違います。";
+
+const questionElement = document.getElementById("question");
+questionElement.innerHTML = question;
+
+const choicesElement = document.getElementById("choices");
+
+choices.forEach(function (choice) {
+
+    var inputElement = document.createElement("input");
+    inputElement.setAttribute("type", "button");
+    inputElement.setAttribute("value", choice);
+    inputElement.addEventListener("click", answer);
+
+    choicesElement.appendChild(inputElement);
+
+});
+
+function answer(e) {
+
+    if (choices[collectIdx] === e.target.value) {
+        alert("OK")
+    } else {
+        alert("NG")
+        e.target.remove();
+    }
+
+
+}
+
+
+
+
+
+
+

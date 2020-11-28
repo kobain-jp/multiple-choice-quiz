8069d76dffd7fd77a027f5714cfefdf4741ecd8a
yoshifumi <yoshifumi@kobain.local>
2020-11-28 19:23:43 +0900

    fix

--- mcquiz-ts/js/built/app.js
+++ mcquiz-ts/js/built/app.js
@@ -1,69 +1,67 @@
 "use strict";
-var McQuize = /** @class */ (function () {
-    function McQuize() {
-        this.quizList = [];
-        this.quiz = { question: "", choices: [], correctIdx: -1, commentary: "" };
-        // html element
-        this.questionEl = document.getElementById("question");
-        this.choicesEl = document.getElementById("choices");
+class McQuize {
+  constructor() {
+    this.quizList = [];
+    this.quiz = { question: "", choices: [], correctIdx: -1, commentary: "" };
+    // html element
+    this.questionEl = document.getElementById("question");
+    this.choicesEl = document.getElementById("choices");
+  }
+  init() {
+    this.loadQuize().then((json) => {
+      this.quizList = json;
+      this.nextQuestion();
+    });
+  }
+  // controller
+  answer(e) {
+    if (parseInt(e.target.dataset.idx) === this.quiz.correctIdx) {
+      alert("正解!!");
+      alert(this.quiz.commentary);
+      this.nextQuestion();
+    } else {
+      alert("不正解!!");
+      e.target.remove();
     }
-    McQuize.prototype.init = function () {
-        var _this = this;
-        this.loadQuize().then(function (json) {
-            _this.quizList = json;
-            _this.nextQuestion();
+  }
+  // controller
+  nextQuestion() {
+    if (this.quizList.length === 0) {
+      alert("おしまい!! お疲れ様でした!!");
+      window.location.reload();
+    }
+    this.quiz = this.quizList.pop();
+    this.renderQuize();
+  }
+  // view
+  renderQuize() {
+    this.questionEl.innerHTML = this.quiz.question;
+    this.choicesEl.innerHTML = "";
+    this.quiz.choices.forEach((choice, idx) => {
+      const liElement = document.createElement("li");
+      liElement.setAttribute("data-idx", idx.toString());
+      liElement.innerHTML = choice;
+      liElement.addEventListener("click", (e) => {
+        this.answer(e);
+      });
+      liElement.classList.add("choice");
+      this.choicesEl.appendChild(liElement);
+    });
+  }
+  //model
+  loadQuize() {
+    return new Promise((resolve, reject) => {
+      fetch("./data.json")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          resolve(data);
+        })
+        .catch((err) => {
+          reject(err);
         });
-    };
-    // controller
-    McQuize.prototype.answer = function (e) {
-        if (parseInt(e.target.dataset.idx) === this.quiz.correctIdx) {
-            alert("正解!!");
-            alert(this.quiz.commentary);
-            this.nextQuestion();
-        }
-        else {
-            alert("不正解!!");
-            e.target.remove();
-        }
-    };
-    // controller
-    McQuize.prototype.nextQuestion = function () {
-        if (this.quizList.length === 0) {
-            alert("おしまい!! お疲れ様でした!!");
-            window.location.reload();
-        }
-        this.quiz = this.quizList.pop();
-        this.renderQuize();
-    };
-    // view
-    McQuize.prototype.renderQuize = function () {
-        var _this = this;
-        this.questionEl.innerHTML = this.quiz.question;
-        this.choicesEl.innerHTML = "";
-        this.quiz.choices.forEach(function (choice, idx) {
-            var liElement = document.createElement("li");
-            liElement.setAttribute("data-idx", idx.toString());
-            liElement.innerHTML = choice;
-            liElement.addEventListener("click", function (e) { _this.answer(e); });
-            liElement.classList.add("choice");
-            _this.choicesEl.appendChild(liElement);
-        });
-    };
-    //model
-    McQuize.prototype.loadQuize = function () {
-        return new Promise(function (resolve, reject) {
-            fetch("./data.json")
-                .then(function (res) {
-                return res.json();
-            })
-                .then(function (data) {
-                resolve(data);
-            })
-                .catch(function (err) {
-                reject(err);
-            });
-        });
-    };
-    return McQuize;
-}());
+    });
+  }
+}
 new McQuize().init();
